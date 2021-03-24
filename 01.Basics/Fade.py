31bf79786d65222a74ba07071f7de7e05f0ef3e5
Hatem-Zehir <76598326+Hatem-Zehir@users.noreply.github.com>
2021-03-24 17:21:31 +0100

    Create Fade.py

---
+++ 01.Basics/Fade.py
@@ -0,0 +1,16 @@
+"""
+  Fade
+
+  This example shows how to fade an LED on GPIO 27
+"""
+
+from machine import Pin, PWM
+from time import sleep
+
+frequency = 5000
+led = PWM(Pin(27), frequency)
+
+while True:
+    for duty_cycle in range(0, 1024):
+        led.duty(duty_cycle)
+        sleep(0.005)
